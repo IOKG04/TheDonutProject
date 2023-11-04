3d487b43504950b2ce6bf087c62ed82ec58de06b
IOKG04 <iokg04@gmail.com>
2023-11-04 18:52:37 +0100

    Halfed resolution in reference code
    I hope this will work with at least 100 ticks per second with some optimized code then.
    
    Other wise ig ill actually have to look in the interpreters code and make it better (or probably easier for me, just make my own interpreter made with speed in mind, and just speed, no debugger n stuff)

--- donuts/donut.dots/liblessdonut.c
+++ donuts/donut.dots/liblessdonut.c
@@ -6,13 +6,13 @@ i,j,k,x,y,o,N;
                  x*=f; \
                  y*=f;
 main(){
-    float z[1760],a=0,e=1,c=1,d=0,f,g,h,G,H,A,t,D;
-    char b[1760];
+    float z[440],a=0,e=1,c=1,d=0,f,g,h,G,H,A,t,D;
+    char b[440];
     for(;;){
-	memset(b,32,1760);
+	memset(b,32,440);
 	g=0;
 	h=1;
-	memset(z,0,7040);
+	memset(z,0,1760);
 	for(j=0;j<90;j++){
 	    G=0;
 	    H=1;
@@ -20,19 +20,19 @@ main(){
 		A=h+2;
 		D=1/(G*A*a+g*e+5);
 		t=G*A*e-g*a;
-		x=40+30*D*(H*A*d-t*c);
-		y=12+15*D*(H*A*c+t*d);
-		o=x+80*y;
+		x=20+15*D*(H*A*d-t*c);
+		y=6+7*D*(H*A*c+t*d);
+		o=x+40*y;
 		N=8*((g*a-G*h*e)*d-G*h*a-g*e-H*h*c);
-		if(22>y&&y>0&&x>0&&80>x&&D>z[o]){
+		if(11>y&&y>-1&&x>0&&40>x&&D>z[o]){
 		    z[o]=D;
 		    b[o]=(N>0?N:0)[".,-~:;=!*#$@"];
 		}
-		R(.02,H,G);
+		R(.04,H,G);
 	    }
-	    R(.07,h,g);
+	    R(.14,h,g);
 	}
-	for(k=0;1761>k;k++) putchar(k%80?b[k]:10);
+	for(k=0;441>k;k++) putchar(k%40?b[k]:10);
 	R(.04,e,a);
 	R(.02,d,c);
 	usleep(15000);
